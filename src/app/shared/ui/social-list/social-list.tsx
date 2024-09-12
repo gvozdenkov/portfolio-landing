9f04869028e5703da7c770fb3c3c1d629c162edf
arty gvozdenkov <gvozdenkov@gmail.com>
2024-09-12 11:09:30 +0300

    fix: social list svg icon gap

--- src/app/shared/ui/social-list/social-list.tsx
+++ src/app/shared/ui/social-list/social-list.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import clsx from 'clsx';
 
 import { SocialItem } from '../../types';
+import { Icon } from '../icons/icon';
 
 import s from './social-list.module.scss';
-import { Icon } from '../icons/icon';
 
 type SocialList = {
   items: SocialItem[];
@@ -15,13 +15,14 @@ export const SocialList = ({ items, extraClass = '' }: SocialList) => {
   return (
     <ul className={clsx(s.socialList, { [extraClass]: !!extraClass })}>
       {items.map((item) => (
-        <li key={item.link}>
+        <li key={item.link} className={s.socialList__item}>
           <Link
             href={item.link}
             title={item.title}
             aria-label={item.title}
             rel="external noreferrer noopener"
             target="_blank"
+            className={s.svgLink}
           >
             <Icon name={item.name} size={30} />
           </Link>
