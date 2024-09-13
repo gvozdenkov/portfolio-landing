4a81736bd325c3014a63c0868825626b7ac7d790
arty gvozdenkov <gvozdenkov@gmail.com>
2024-09-13 14:44:45 +0300

    refactor: tune contacts section responsive

--- src/app/components/contacts/contacts.tsx
+++ src/app/components/contacts/contacts.tsx
@@ -11,12 +11,9 @@ type ContactsProps = {
 
 export const Contacts = ({ extraClass = '' }: ContactsProps) => {
   return (
-    <section
-      id="contact"
-      className={clsx('padding-block-100', s.contacts, { [extraClass]: !!extraClass })}
-    >
-      <div className="container">
-        <Logo solid />
+    <section id="contact" className={clsx('padding-block-100', { [extraClass]: !!extraClass })}>
+      <div className={clsx('container', s.contacts)}>
+        <Logo solid extraClass={s.contacts__logo} />
         <p className={clsx(s.contacts__text, s.contacts__text_phone)}>+91 12345 09876</p>
         <p className={clsx(s.contacts__text, s.contacts__text_email)}>info@example.com</p>
         <SocialList items={socialItems} extraClass={s.contacts__social} />
